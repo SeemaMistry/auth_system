338ca2dcb1157221115a14f41054e862e71db752
SeemaMistry <seema.m.1996@gmail.com>
2023-02-15 21:05:41 -0500

    feat: initialize UserAccountsManager to handle user creation

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+# create Manager to handle creating a new User
+class UserAccountManager(BaseUserManager):
+    # override create_user() 
+    def create_user(self, email, name, password=None):
+        # raise error when no email set
+        if not email:
+            raise ValueError('Users must have an email address')
+        # set user and save
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        # store password as a hash
+        user.set_password(password)
+        user.save()
+
+        return user
+
+# create UserAccount with email as username
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)

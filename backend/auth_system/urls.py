2253f047803ae8346002757e9df194e3411d14f0
SeemaMistry <seema.m.1996@gmail.com>
2023-02-19 17:33:59 -0500

    feat: add repath for template views to be used with React

--- backend/auth_system/urls.py
+++ backend/auth_system/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt'))
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]

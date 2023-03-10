24f5bdd5b919485fc341d40f0e63c6d20adc447d
SeemaMistry <seema.m.1996@gmail.com>
2023-03-09 20:14:12 -0500

    fix: correct typo in templates DIRs

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'auth_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.joing(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
